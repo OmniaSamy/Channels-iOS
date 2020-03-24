e4de5ce5c0c48e001a6836150be9d41287e8649b
Omnia Samy <omnia.samyabdelmoneam@gmail.com>
2020-03-24 09:26:13 +0200

    1- add .gitignore 2- create new project

---
+++ Channels/Channels/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Channels
+//
+//  Created by Omnia Samy on 3/23/20.
+//  Copyright © 2020 Omnia Samy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
